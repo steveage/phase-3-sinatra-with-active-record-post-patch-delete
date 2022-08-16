9d6aad88a906d0140c017db95b0f45d617fe3d9c
Mariusz <mariuszbia@yahoo.com>
2022-08-16 08:32:21 -0500

    Completed assignment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get '/games' do
-    games = Game.all.order(:title).limit(10)
-    games.to_json
+    Game.all.to_json
   end
 
   get '/games/:id' do
@@ -16,4 +17,28 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
